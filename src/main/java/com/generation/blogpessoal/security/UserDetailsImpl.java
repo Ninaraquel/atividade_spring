9c1a670801624847f728cb3c2212709b860c253a
Ninaraquel <nynaferreira.souza@gmail.com>
2024-12-06 17:24:48 -0300

    atualização

--- src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
@@ -39,24 +39,4 @@ public class UserDetailsImpl implements UserDetails {
 		return userName;
 	}
 
-//	@Override
-//	public boolean isAccountNonExpired() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isAccountNonLocked() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isCredentialsNonExpired() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isEnabled() {
-//		return true;
-//	}
-
 }
